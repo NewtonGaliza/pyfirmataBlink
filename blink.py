fcd51db3723b63fc5b68ece1a3c587b9c8cc4f5b
Newton Galiza <newtonjgaliza@gmail.com>
2018-03-29 15:11:38 -0300

    Update blink.py

--- blink.py
+++ blink.py
@@ -4,7 +4,7 @@ import time
 
 board = Arduino('/dev/tty/USB0')
 for i in range(10):
-              board.digital[13].write(1)
-               time.sleep(1)
-               board.digital[13].write(0)
-               time.sleep(1)
+    board.digital[13].write(1)
+    time.sleep(1)
+    board.digital[13].write(0)
+    time.sleep(1)
